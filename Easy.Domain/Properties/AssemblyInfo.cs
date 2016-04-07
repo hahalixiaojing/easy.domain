7b999bcd67449aa1190f9ace934183fc1d6f2621
lixiaojing <lixiaojing@etaostars.com>
2016-04-07 13:52:50 +0800

    提交文件

--- Easy.Domain/Properties/AssemblyInfo.cs
+++ Easy.Domain/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompanyAttribute("李晓静")]
 [assembly: AssemblyProduct("Easy.Domain")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // 可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.1.7")]
-[assembly: AssemblyFileVersion("2.0.1.7")]
+[assembly: AssemblyVersion("2.0.1.8")]
+[assembly: AssemblyFileVersion("2.0.1.8")]
